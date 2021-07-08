4bf00d269b31b66998f26988d38882b4a88faf7d
Erling Erlingsson <erling@taktikal.is>
2021-07-08 11:21:23 +0000

    Update navigation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,12 +17,56 @@ module.exports = {
         srcDark: 'logo-light.svg',
       },
       items: [
+        { to: '/docs/fill-and-sign', label: 'Fill & Sign', position: 'left' },
+        { to: '/docs/drop-and-sign', label: 'Drop & Sign', position: 'left' },
+        { to: '/docs/smart-forms', label: 'Smart Forms', position: 'left' },
+        { to: '/docs/api', label: 'API', position: 'left' },
         {
           href: 'https://gitlab.com/taktikal/docs',
           label: 'GitLab',
           position: 'right',
         },
       ],
+    },
+    prism: { additionalLanguages: ['csharp', 'php'] },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Drop & Sign',
+              to: '/docs/drop-and-sign',
+              activeBasePath: 'docs/drop-and-sign',
+            },
+            {
+              label: 'Fill & Sign',
+              to: '/docs/fill-and-sign',
+              activeBasePath: 'docs/fill-and-sign',
+            },
+            {
+              label: 'Smart Forms',
+              to: '/docs/smart-forms',
+              activeBasePath: 'docs/smart-forms',
+            },
+            {
+              label: 'API',
+              to: '/docs/api',
+              activeBasePath: 'docs/api',
+            },
+          ],
+        },
+        {
+          items: [
+            {
+              label: 'Taktikal',
+              href: 'https://taktikal.is',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Taktikal Docs Built with Docusaurus.`,
     },
     algolia: {
       apiKey: 'ce54ae98420beac5aa8e3ad58d7786ce', // Read access key
@@ -34,8 +78,7 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.ts'),
           editUrl: 'https://gitlab.com/taktikal/docs/-/edit/master',
         },
         theme: {
